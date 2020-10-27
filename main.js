f834ff3d2b0e9a4718f838138fec5af04d720002
pbaker123 <p.baker1@yahoo.com>
2020-10-27 13:17:52 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const selectors = {
+  body: document.querySelector("body"),
+};
+
+const buttons = ["saveReturn","cancelA","save","cancelB","resetDefault","optionViews"];
+
+selectors.body.addEventListener("click", function(event) {
+  const classname = event.explicitOriginalTarget.className;
+  if (buttons.includes(classname)) {
+    showLabel(classname);
+    showDetail(classname);
+    addBorder(classname);
+    return;
+  };
+  return;
+});
+
+function showLabel (classname) {
+  for (const button of buttons) {
+    document.querySelector("." + button + "Label").style.display = "none";
+  }
+  document.querySelector("." + classname + "Label").style.display = "block";
+};
+
+function showDetail (classname) {
+  for (const button of buttons) {
+    document.querySelector("." + button + "Detail").style.display = "none";
+  }
+  document.querySelector("." + classname + "Detail").style.display = "block";
+};
+
+function addBorder (classname) {
+  for (const button of buttons) {
+    document.querySelector("." + button).style.border = "";
+  }
+  document.querySelector("." + classname).style.border = "2px solid #E61E1E";
+};
